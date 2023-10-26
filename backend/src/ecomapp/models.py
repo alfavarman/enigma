e0f3463f5ba498cc5f1e3866c85bba12fc49b680
CKS <communicate2charles@gmail.com>
2023-10-27 02:51:53 +0700

    orders

--- backend/src/ecomapp/models.py
+++ backend/src/ecomapp/models.py
@@ -25,9 +25,30 @@ class Product(models.Model):
 
 class Order(models.Model):
     """Order model"""
-    pass
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Customer")
+    delivery_address = models.TextField(verbose_name="Delivery Address")
+    order_date = models.DateTimeField(auto_now_add=True, verbose_name="Order Date")
+    payment_due_date = models.DateTimeField(verbose_name="Payment Due Date")
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Total Price")
+
+    # This will be a many-to-many relationship to handle products and their quantities
+    products = models.ManyToManyField(Product, through='OrderProduct', verbose_name="Products")
+
+    def __str__(self):
+        return f"Order {self.id} by {self.customer.username}"
 
 
 class OrderProduct(models.Model):
     """Quantity of product for a given order."""
-    pass
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, verbose_name="Order")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name="Product")
+    quantity = models.PositiveIntegerField(verbose_name="Quantity")
+
+    def __str__(self):
+        return f"{self.product.name} x {self.quantity}"
+
+
+
+### TODO : COMMENT: 1) django inherit the id fields from the parent class,
+# however for some cases like products where ids will be generated, added, removed etc we would use UUID insted
+# of autoincrement primary key. As for simplicity of project SQL lite is used which stores UUID as TEXT todo is marked.
