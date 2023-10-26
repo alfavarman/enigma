7e8e18f4043a2e6ce29210e9aa27e09689e1e4fd
CKS <communicate2charles@gmail.com>
2023-10-27 02:30:55 +0700

    model plan

--- backend/src/ecomapp/models.py
+++ backend/src/ecomapp/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    """Product category"""
+    pass
+
+
+class Product(models.Model):
+    """Product model"""
+    pass
+
+
+class Order(models.Model):
+    """Order model"""
+    pass
+
+
+class OrderProduct(models.Model):
+    """Quantity of product for a given order."""
+    pass
+
